a4eb33b3038d9374f5e54565c41f798d42102ed1
ashis34 <88176690+ashis34@users.noreply.github.com>
2025-09-20 23:02:39 +0530

    Update first-dag.py
    ds

--- plarform-dags-aggregator/demo-dag/first-dag.py
+++ plarform-dags-aggregator/demo-dag/first-dag.py
@@ -8,3 +8,31 @@ default_args = {
     'retries': 5,
     'retry_delay': timedelta(minutes=2)
 }
+
+with DAG(
+    dag_id='our_first_dag',
+    default_args=default_args,
+    description='This is our first dag',
+    start_date= days_ago(2),
+    schedule_interval=None,
+    #start_date=datetime(2023, 9, 12, 2),
+    #schedule_interval='@daily'
+    access_control={
+        'role_ashis123': {
+            'can_read',
+            'can_edit',
+            'can_delete'
+        },
+        'role_test': {
+            'can_read',
+            'can_edit',
+            'can_delete'
+        }
+}
+) as dag: 
+    task1=BashOperator(
+        task_id='first_task',
+        bash_command="echo Hello first task; echo New code"
+    )
+
+    task1
